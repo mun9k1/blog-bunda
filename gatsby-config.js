6515c3272c4d09f6fc694eefa89768a23993d4ec
Mungki Sulistiono <mun9k1@Mungkis-MacBook-Pro.local>
2018-11-05 15:18:03 +0700

    inital commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'KelasIbuBelajar',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Belajar Berpikir Positif</h1>
+    <p>KelasIbuBelajar adalah adalah tempat para ibu menyalurakan pikiran dan <br/>
+      energi untuk lebih positif</p>
+    <Link to="/page-2/">Get Started!</Link>
+  </div>
+)
+
+export default IndexPage
