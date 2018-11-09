326b4ebd091b7b2f1da96ccdd77aa451a587e695
mun9k1 <mv9k1@gmail.com>
2018-11-09 09:28:59 +0700

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,18 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Belajar Berpikir Positif</h1>
-    <p>KelasIbuBelajar adalah adalah tempat para ibu menyalurakan pikiran dan <br/>
-      energi untuk lebih positif</p>
-    <Link to="/page-2/">Get Started yuk!</Link>
+
+    <div classname="Hero">
+
+      <div classname="HeroGroup">
+
+        <h1>Belajar Berpikir Positif</h1>
+        <p>KelasIbuBelajar adalah adalah tempat para ibu menyalurakan pikiran dan <br/>
+        energi untuk lebih positif</p>
+        <Link to="/page-2/">Get Started yuk!</Link>
+
+      </div>
+    </div>
   </div>
 )
 
